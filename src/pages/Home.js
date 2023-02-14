71b0bb8958898e80dadbf9f48e06ea2745227b50
Qianqian-Bella <bellahahaha@bellahahahas-MacBook-Air.local>
2023-02-14 12:27:30 -0800

    Feat: add search bar in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Headers";
+import { Form, Input, Button } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
 const Home = () => {
   return (
     <div className="home">
-      <Header/>
+      <Header />
       <div
         style={{
           fontSize: "55px",
@@ -30,12 +32,43 @@ const Home = () => {
         <div
           style={{
             fontSize: "20px",
+            color: "rgba(105, 185, 157, 0.8)",
+            textAlign: "left",
+            marginTop: "50px",
+            marginLeft: " 200px",
+          }}
+        >
+          <b>Rentals Location</b>
+        </div>
+        <div
+          style={{
+            fontSize: "20px",
             textAlign: "left",
             marginTop: "20px",
             marginLeft: " 200px",
           }}
         >
-          Rental Location
+          <Form
+            name="customized_form_controls"
+            layout="inline"
+            // onFinish={onSearch}
+          >
+            <Form.Item name="filter" style={{ width: 500 }}>
+              <Input placeholder="Enter an address, city or zip code" />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                htmlType="submit"
+                style={{
+                  color: "white",
+                  backgroundColor: "rgba(105, 185, 157, 0.8)",
+                }}
+                icon={<SearchOutlined />}
+              >
+                Search
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     </div>
