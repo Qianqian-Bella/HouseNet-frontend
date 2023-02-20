2e81b41e11a97866dc8e146aa3447443efbfefc6
Brian Wu <15759684259@163.com>
2023-02-20 15:57:45 -0800

    signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,105 +1,136 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Form, Input, Button, message, Checkbox } from "antd";
-import { CheckboxChangeEvent } from "antd/es/checkbox";
 import Header from "../components/Headers";
 import axios from "axios";
 
-const Signin = () => {
-  const [form] = Form.useForm();
-  const [loading, setLoading] = useState(false);
-  const [checked, setChecked] = useState(false);
-  const [disabled, setDisabled] = useState(false);
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "brian",
+      password: "123456",
+      loading: false,
+      checked: false,
+      disabled: false,
+    };
+    this.label = `${this.state.checked ? "Remembered!" : "Remember me"}`;
+    this.toggleDisable = this.toggleDisable.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleUsenameChange = this.handleUsenameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.onFinish = this.onFinish.bind(this);
+  }
 
-  const toggleChecked = () => {
-    setChecked(!checked);
-  };
+  toggleDisable() {
+    this.setState({ disabled: !this.state.disabled });
+  }
 
-  const toggleDisable = () => {
-    setDisabled(!disabled);
-  };
+  handleCheckboxChange(event) {
+    console.log("checked = ", event.target.checked);
+    this.setState({ checked: event.target.checked });
+  }
 
-  const onChange = (e: CheckboxChangeEvent) => {
-    console.log("checked = ", e.target.checked);
-    setChecked(e.target.checked);
-  };
-
-  const label = `${checked ? "Remembered!" : "Remember me"}`;
-
-  const onFinish = (values) => {
-    setLoading(true);
-    // Perform registration logic here, e.g. API call to backend
+  handleUsenameChange(event) {
+    console.log("username = ", event.target.value);
+    this.setState({ username: event.target.value });
+  }
+  handlePasswordChange(event) {
+    console.log("password = ", event.target.value);
+    this.setState({ password: event.target.value });
+  }
+  onFinish(values) {
+    this.setState({ loading: true });
     axios
       .post("/api/register", values)
       .then(() => {
         message.success("Registration successful!");
-        setLoading(false);
-        // history.push("/");
+        this.setState({ loading: false });
       })
       .catch((error) => {
         message.error("Registration failed. Please try again.");
-        setLoading(false);
+        this.setState({ loading: false });
         console.error(error);
       });
-  };
+  }
 
-  // const { getFieldDecorator } = this.props.form;
-  return (
-    <div className="signin">
-      <Header />
-      <div
-        className="signin-form"
-        style={{ maxWidth: 400, margin: "auto", marginTop: 100 }}
-      >
-        <h1>Sign in an account</h1>
-        <Form form={form} layout="vertical" onFinish={onFinish}>
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-              { required: true, message: "Please input your username!" },
-              { min: 4, message: "Username must be at least 4 characters" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              { required: true, message: "Please input your password!" },
-              { min: 8, message: "Password must be at least 8 characters" },
-            ]}
-          >
-            <Input.Password />
-            <Checkbox checked={checked} disabled={disabled} onChange={onChange}>
-              {label}
-            </Checkbox>
-            {/* <Button type="primary" size="small" onClick={toggleChecked}>
-              {!checked ? "Check" : "Uncheck"}
-            </Button> */}
-            <Button
-              style={{ margin: "0 10px" }}
-              type="primary"
-              size="small"
-              onClick={toggleDisable}
+  render() {
+    return (
+      <div className="signin">
+        <Header />
+        <div
+          className="signin-form"
+          style={{ maxWidth: 400, margin: "auto", marginTop: 100 }}
+        >
+          <h1>Sign in an account</h1>
+          <Form layout="vertical" onFinish={this.onFinish}>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+                { min: 4, message: "Username must be at least 4 characters" },
+              ]}
             >
-              {!disabled ? "Disable" : "Enable"}
-            </Button>
-            <a className="login-form-forgot" href="/sign-up">
-              Forgot password
-            </a>
-          </Form.Item>
+              <Input
+                name="userName"
+                placeholder={this.state.username}
+                size="large"
+                onChange={this.handleUsenameChange}
+              />
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading}>
-              Sign in
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+                { min: 8, message: "Password must be at least 8 characters" },
+              ]}
+            >
+              <Input.Password
+                name="password"
+                placeholder={this.state.password}
+                size="large"
+                onChange={this.handlePasswordChange}
+              />
+
+              <Checkbox
+                checked={this.state.checked}
+                disabled={this.state.disabled}
+                onChange={this.handleCheckboxChange}
+              >
+                {`${this.state.checked ? "Remembered!" : "Remember me"}`}
+              </Checkbox>
+
+              <Button
+                style={{ margin: "0 5px" }}
+                type="primary"
+                size="small"
+                onClick={this.toggleDisable}
+              >
+                {!this.state.disabled ? "Disable" : "Enable"}
+              </Button>
+
+              <a className="login-form-forgot" href="/sign-up">
+                Forgot password
+              </a>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                loading={this.state.loading}
+                size="large"
+              >
+                Sign in
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Signin;
